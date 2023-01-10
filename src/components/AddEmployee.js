faa43e599abfb571bf2e5ca1e0784c8ee1c84117
Jason Chaskin <jason.chaskin@gmail.com>
2023-01-09 22:31:37 -0500

    still working on this but pushing what I've done so far

---
+++ src/components/AddEmployee.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react"
+import Modal from "react-bootstrap/Modal"
+
+export default function EditEmployee(props) {
+    const [show, setShow] = useState(false)
+    const [inputValueName, setInputValueName] = useState("")
+    const [inputValueRole, setInputValueRole] = useState("")
+    const [inputValueImageUrl, setInputValueImageUrl] = useState("")
+
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+
+    return (
+        <>
+            <button
+                onClick={handleShow}
+                className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded mx-auto block m-2"
+            >
+                + Add Employee
+            </button>
+
+            <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Employee</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form
+                        id="editmodal"
+                        className="w-full max-w-sm"
+                        onSubmit={(e) => {
+                            e.preventDefault()
+                            props.addChange(inputValueName, inputValueRole, inputValueImageUrl)
+                            setInputValueName("")
+                            setInputValueRole("")
+                            setInputValueImageUrl("")
+                            handleClose()
+                        }}
+                    >
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label
+                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    htmlFor="name"
+                                >
+                                    Full Name
+                                </label>
+                            </div>
+                            <div className="md:w-2/3">
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="name"
+                                    type="text"
+                                    value={inputValueName}
+                                    onChange={(e) => setInputValueName(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label
+                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    htmlFor="role"
+                                >
+                                    Role
+                                </label>
+                            </div>
+                            <div className="md:w-2/3">
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="role"
+                                    type="text"
+                                    value={inputValueRole}
+                                    onChange={(e) => setInputValueRole(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div className="md:flex md:items-center mb-6">
+                            <div className="md:w-1/3">
+                                <label
+                                    className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                                    htmlFor="img"
+                                >
+                                    Image URL
+                                </label>
+                            </div>
+                            <div className="md:w-2/3">
+                                <input
+                                    className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                    id="img"
+                                    type="text"
+                                    value={inputValueImageUrl}
+                                    onChange={(e) => setInputValueImageUrl(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <button
+                        className="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded"
+                        onClick={handleClose}
+                    >
+                        Close
+                    </button>
+                    <button
+                        className="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+                        form="editmodal"
+                    >
+                        Add
+                    </button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
